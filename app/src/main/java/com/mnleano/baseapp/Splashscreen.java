b9c7187cf0c5b64450492352deb49b1e0652dbbd
Mykel Neds Leaño <mykel.leano@mcpayment.net>
2016-02-29 09:39:36 +0800

    First Commit

---
+++ app/src/main/java/com/mnleano/baseapp/Splashscreen.java
@@ -0,0 +1,14 @@
+package com.mnleano.baseapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+    }
+}
